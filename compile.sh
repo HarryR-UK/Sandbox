a4656e52a07b2702b52b8ce29d5703bcf915686f
WindowBender <justcallitaday@outlook.com>
2023-07-25 16:47:11 +0100

    added falling sand

--- compile.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-g++ -std=c++20 src/*.cpp -o build/SandSimulation -I/opt/homebrew/Cellar/sfml/2.5.1_2/include -L/opt/homebrew/Cellar/sfml/2.5.1_2/lib -lsfml-system -lsfml-window -lsfml-graphics -lsfml-audio -lsfml-network
